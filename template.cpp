a42a6681ac48a405c37a8cd53f94060dbd6877ff
beidou2023 <131274568+beidou2023@users.noreply.github.com>
2025-04-17 18:15:05 -0400

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int                 ll
+#define mp                  make_pair
+#define pb                  push_back
+#define all(a)              (a).begin(), (a).end()
+#define sz(a)               (int)a.size()
+#define mod(a)              md(a, MOD)
+#define srt(a)              sort(all(a))
+#define mem(a, h)           memset(a, (h), sizeof(a))
+#define forn(i, n)          for(int i = 0; i < n; i++)
+#define fore(i, b, e)       for(int i = b; i < e; i++)
+#define forev(i, e, b)      for(int i = e; i > b; i--)
+#define forg(i, b, e, m)    for(int i = b; i < e; i+=m)
+#define index               int mid = (b + e) / 2, l = node * 2 + 1, r = l + 1;
+#define DBG(x)              cerr<<#x<<" = "<<(x)<<endl
+#define RAYA                cout<<"=============================="<<'\n'
+#define fast                ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+typedef long long                   ll;
+typedef long double                 ld; 
+typedef unsigned long long          ull;
+typedef pair<int, int>              ii;
+typedef pair<pair<int, int>, int>   iii;
+typedef vector<int>                 vi;
+typedef vector<ii>                  vii;
+typedef vector<ll>                  vll;
+
+const int tam       =   200010;
+const int MOD       =   1000000007;
+const int MOD1      =   998244353;
+const double DINF   =   1e100;
+const double EPS    =   1e-9;
+const double PI     =   acos(-1);
+
+int cases   =   0;
+
+void solve(){
+
+    // cout<<"Case "<<cases<<":"<<endl;
+}
+
+signed main(){
+    fast;
+    int tt=1;
+    //cin>>tt;
+    while(tt--){
+        cases++;
+        solve();
+    }
+    return 0;
+}
