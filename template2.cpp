7335ce46c269298063cf85915da6c44b34039a20
beidou2023 <131274568+beidou2023@users.noreply.github.com>
2025-04-26 20:56:34 -0400

    Create template2.cpp

---
+++ template2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int                 ll
+#define fast                ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+typedef long long                   ll;
+
+signed main(){
+    fast;
+
+    return 0;
+}
